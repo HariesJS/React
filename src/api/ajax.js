70de7118dc6fd8be3e97c17aed96ca4e94d62e23
Evgeniy Chepurnoy <evgeniychepurnoy@Evgeniys-MacBook-Pro.local>
2020-01-07 00:00:59 +0200

    haries network 1.0

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        'API-KEY': '11452ed1-3660-45e9-b8d1-a254f78a41b8_'
+        'API-KEY': '11452ed1-3660-45e9-b8d1-a254f78a41b8'
     }
 })
 
